fed5122c42c6a7d33fd10fe9d4ed12b47dae0915
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 16:53:57 -0500

    Added headers in interface columns base, and constrains base

--- AnyDataBaseFluent/ConstraintBase.cs
+++ AnyDataBaseFluent/ConstraintBase.cs
@@ -1,9 +1,11 @@
 ﻿using AnyDataBaseFluent.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnyDataBaseFluent.EnumTypes;
 
 namespace AnyDataBaseFluent
 {
