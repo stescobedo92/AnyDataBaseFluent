36ff0c518af107de52e48a79d9266aadcfbcec95
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 15:47:11 -0500

    Added the constrins type

---
+++ AnyDataBaseFluent/EnumTypes/ConstraintType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyDataBaseFluent.EnumTypes
+{
+    public enum ConstraintType
+    {
+        PRIMARY_KEY = 1,
+        FOREIGN_KEY = 2,
+        UNIQUE = 3,
+        CHECK = 4,
+        NOT_NULLNot
+    }
+}
