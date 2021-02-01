ec5a1765f9ca2f2fe77b362a2c94ed009e8a45b2
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 16:51:45 -0500

    Added headers

--- AnyDataBaseFluent/Access/AnyDatabaseAccessException.cs
+++ AnyDataBaseFluent/Access/AnyDatabaseAccessException.cs
@@ -1,12 +1,15 @@
 ﻿#region License
 // Copyright 2021 Sergio Triana Escobedo
 #endregion
+
+using AnyDataBaseFluent.Exception;
+
 namespace AnyDatabase.Access
 {
 	/// <summary>
 	/// Represents errors that occur when creating a Access database.
 	/// </summary>
-	public class AnyDatabaseAccessException : AnyDatabaseException
+	public class AnyDatabaseAccessException : AnyDatabaseFluentDatabaseException
 	{
 		/// <summary>
 		/// Creates a new instance of FluentDatabaseAccessException.
