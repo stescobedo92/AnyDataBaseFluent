458b1f7f04ca45870810ba80ca580c392acb4653
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 15:14:12 -0500

    Added sqlite configuration files

---
+++ AnyDataBaseFluent/Sqlite/AnyDataBaseFluentSqliteException.cs
@@ -0,0 +1,17 @@
+﻿#region License
+// Copyright 2021 Sergio Triana Escobedo
+#endregion
+namespace AnyDataBaseFluent.Firebird
+{
+	/// <summary>
+	/// Represents errors that occur when creating a Firebird database.
+	/// </summary>
+	public class AnyDataBaseFluentSqliteException : AnyDataBaseFluentException
+	{
+		/// <summary>
+		/// Creates a new instance of FluentDatabaseFirebirdException.
+		/// </summary>
+		/// <param name="message"></param>
+		public AnyDataBaseFluentSqliteException( string message ) : base( message ) { }
+	}
+}
---
+++ AnyDataBaseFluent/Sqlite/Constraint.cs
@@ -0,0 +1,15 @@
+﻿#region License
+// Copyright 2021 Sergio Triana Escobedo
+#endregion
+using System;
+using System.IO;
+
+namespace AnyDataBaseFluent.Firebird
+{
+	public class Constraint : ConstraintBase
+	{
+		public override void Write( StreamWriter writer )
+		{
+		}
+	}
+}
