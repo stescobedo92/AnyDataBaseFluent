efa5b8d0f67e8451d6dbbba83aaefc1895696691
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 16:16:42 -0500

    Added database exception

---
+++ AnyDataBaseFluent/Exception/AnyDatabaseFluentDatabaseException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyDataBaseFluent.Exception
+{
+	/// <summary>
+	/// Represents errors that occur when using FluentDatabase.
+	/// </summary>
+	class AnyDatabaseFluentDatabaseException : Exception
+    {
+        /// <summary>
+		/// Creates a new instance of FluentDatabaseException.
+		/// </summary>
+		/// <param name="message">The error message.</param>
+		public AnyDatabaseFluentDatabaseException(string message) : base(message) { }
+    }
+}
