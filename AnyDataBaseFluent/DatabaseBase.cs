b0412abb4bf7512d24d4bcfe5955a5794702c824
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 15:54:44 -0500

    Added the database base class

---
+++ AnyDataBaseFluent/DatabaseBase.cs
@@ -0,0 +1,57 @@
+﻿using AnyDataBaseFluent.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyDataBaseFluent
+{
+    public abstract class DatabaseBase : IDatabase
+    {
+		protected string Name { get; set; }
+		protected string Schema { get; set; }
+		protected List<ITable> Tables { get; set; }
+
+		protected abstract ITable CreateTable();
+		protected abstract void WriteUse(StreamWriter writer);
+
+		protected DatabaseBase()
+		{
+			Tables = new List<ITable>();
+			Schema = "dbo";
+		}
+
+		public IDatabase WithName(string name)
+		{
+			Name = name;
+			return this;
+		}
+
+		public IDatabase UsingSchema(string schema)
+		{
+			Schema = schema;
+			return this;
+		}
+
+		public IDatabase HasTable(Action<ITable> table)
+		{
+			var newTable = CreateTable();
+			table.Invoke(newTable);
+			Tables.Add(newTable);
+			return this;
+		}
+
+		public void Write(StreamWriter writer)
+		{
+			WriteUse(writer);
+			writer.WriteLine();
+			foreach (var table in Tables)
+			{
+				table.UsingSchema(Schema);
+				table.Write(writer);
+				writer.WriteLine();
+			}
+		}
+	}
+}
