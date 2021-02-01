51246b0ea981c97cf82f2939d214d57f26bb4433
Sergio Triana <sergio.triana@ntsprint.com>
2021-02-01 15:15:00 -0500

    Added sqlserver configurations files

---
+++ AnyDataBaseFluent/SqlServer/Column.cs
@@ -0,0 +1,20 @@
+﻿#region License
+// Copyright 2021 Sergio Triana Escobedo
+#endregion
+using System;
+using System.IO;
+
+namespace AnyDataBaseFluent.Firebird
+{
+	public class Column : ColumnBase
+	{
+		protected override void WriteColumn( StreamWriter writer )
+		{
+		}
+
+		protected override IConstraint CreateConstraint()
+		{
+			return new Constraint();
+		}
+	}
+}
